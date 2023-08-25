e921ddf1c74c59b561b9ac18c62792a76e2cb0c2
spargel125 <asupar125ce@gmail.com>
2023-08-26 00:07:27 +0900

    cとs追加

--- src/uniform.rs
+++ src/uniform.rs
@@ -1,3 +1,4 @@
+//! `strust Uniform`
 pub struct Uniform<T, U> {
     l: T,
     u: U,
@@ -11,16 +12,28 @@ where
     pub fn new(l: T, u: U) -> Self {
         Self { l: l, u: u }
     }
-
+    /// calc E[x]
     pub fn x(&self) -> f64 {
         let u = self.u.into();
         let l = self.l.into();
         (u + l) / (2.0f64)
     }
-
+    /// calc E[x^2]
     pub fn xx(&self) -> f64 {
         let u = self.u.into();
         let l = self.l.into();
-        (u*u + u*l + l*l) / 3.0f64
+        (u * u + u * l + l * l) / 3.0f64
+    }
+    /// calc E[cos(x)]
+    pub fn c(&self) -> f64 {
+        let u = self.u.into();
+        let l = self.l.into();
+        (u.sin() - l.sin()) / (u - l)
+    }
+    // calc E[sin(x)]
+    pub fn s(&self) -> f64 {
+        let u = self.u.into();
+        let l = self.l.into();
+        -(u.cos() - l.cos()) / (u - l)
     }
 }
--- tests/uniform_test.rs
+++ tests/uniform_test.rs
@@ -11,6 +11,8 @@ mod tests_uniform {
         let uniform_int = Uniform::new(3, 9);
         assert!(uniform_int.x() - 6.0 < 1e-12);
         assert!(uniform_int.xx() - 39.0 < 1e-12);
+        assert!(uniform_int.c() - 0.045166412863648 < 1e-12);
+        assert!(uniform_int.s() - (-0.013143705785961) < 1e-12);
     }
     #[test]
     fn test_float() {
@@ -18,6 +20,8 @@ mod tests_uniform {
         let uniform_float = Uniform::new(-2.1, 5.4);
         assert!(uniform_float.x() - 1.65 < 1e-12);
         assert!(uniform_float.xx() - 7.41 < 1e-12);
+        assert!(uniform_float.c() - 0.012059317212385 < 1e-12);
+        assert!(uniform_float.s() - (-0.151938530738999) < 1e-12);
     }
     #[test]
     fn test_int_float() {
@@ -25,5 +29,7 @@ mod tests_uniform {
         let uniform_int_float = Uniform::new(-13.3, 11);
         assert!(uniform_int_float.x() - (-1.15) < 1e-12);
         assert!(uniform_int_float.xx() - 50.53 < 1e-12);
+        assert!(uniform_int_float.c() - 0.013597549150375 < 1e-12);
+        assert!(uniform_int_float.s() - 0.030383682087062 < 1e-12);
     }
 }
