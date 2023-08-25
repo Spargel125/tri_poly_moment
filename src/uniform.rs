0510fb41b41a8f315d0b919c656ec71982bf9f3e
spargel125 <asupar125ce@gmail.com>
2023-08-25 23:41:58 +0900

    test記述変更

--- src/uniform.rs
+++ src/uniform.rs
@@ -1,19 +1,26 @@
-pub struct Uniform<T,U> {
-    u:T,
-    l:U,
+pub struct Uniform<T, U> {
+    l: T,
+    u: U,
 }
 
-impl<T,U> Uniform<T,U>
-where 
-    T:Into<f64> + Copy,
-    U:Into<f64> + Copy,
+impl<T, U> Uniform<T, U>
+where
+    T: Into<f64> + Copy,
+    U: Into<f64> + Copy,
 {
-    pub fn new(u:T,l:U) -> Self{
-        Self { u: u, l: l }
+    pub fn new(l: T, u: U) -> Self {
+        Self { l: l, u: u }
     }
 
-    pub fn x(&self) -> f64{
-        (self.u.into()+self.l.into())/(2.0f64)
+    pub fn x(&self) -> f64 {
+        let u = self.u.into();
+        let l = self.l.into();
+        (u + l) / (2.0f64)
+    }
+
+    pub fn xx(&self) -> f64 {
+        let u = self.u.into();
+        let l = self.l.into();
+        (u*u + u*l + l*l) / 3.0f64
     }
 }
-
--- tests/uniform_test.rs
+++ tests/uniform_test.rs
@@ -2,17 +2,28 @@ extern crate tri_poly_moment;
 
 use tri_poly_moment::Uniform;
 
-#[test]
-fn test_uniform_x() {
-    // Test case 1
-    let uniform_int = Uniform::new(10,2);
-    assert_eq!(uniform_int.x(), 6.0);
-
-    // Test case 2
-    let uniform_float = Uniform::new(5.3,-2.0);
-    assert_eq!(uniform_float.x(), 1.65);
-
-    // Test case 3
-    let uniform_int_float = Uniform::new(5,1.0);
-    assert_eq!(uniform_int_float.x(), 3.0);
+#[cfg(test)]
+mod tests_uniform {
+    use super::*;
+    #[test]
+    fn test_int() {
+        // Test case 1
+        let uniform_int = Uniform::new(3, 9);
+        assert!(uniform_int.x() - 6.0 < 1e-12);
+        assert!(uniform_int.xx() - 39.0 < 1e-12);
+    }
+    #[test]
+    fn test_float() {
+        // Test case 2
+        let uniform_float = Uniform::new(-2.1, 5.4);
+        assert!(uniform_float.x() - 1.65 < 1e-12);
+        assert!(uniform_float.xx() - 7.41 < 1e-12);
+    }
+    #[test]
+    fn test_int_float() {
+        // Test case 3
+        let uniform_int_float = Uniform::new(-13.3, 11);
+        assert!(uniform_int_float.x() - (-1.15) < 1e-12);
+        assert!(uniform_int_float.xx() - 50.53 < 1e-12);
+    }
 }
