e6c7a29bf3f30c29e756391097ab31759780be48
spargel125 <asupar125ce@gmail.com>
2023-08-26 20:02:23 +0900

    docの例追加

--- src/uniform.rs
+++ src/uniform.rs
@@ -9,28 +9,40 @@ where
     T: Into<f64> + Copy,
     U: Into<f64> + Copy,
 {
+    /// define new `Uniform` distribution
+    ///
+    /// ## Arguments
+    /// * `l` : lower of uniform distribution
+    /// * `u` : upper of uniform distribution
+    ///
+    /// ## Example
+    /// ```
+    /// use tri_poly_moment::Uniform;
+    /// let uniform_int_float = Uniform::new(-13.3, 11);
+    /// ```
+
     pub fn new(l: T, u: U) -> Self {
         Self { l: l, u: u }
     }
-    /// calc E[x]
+    /// calc `E[x]`
     pub fn x(&self) -> f64 {
         let u = self.u.into();
         let l = self.l.into();
         (u + l) / (2.0f64)
     }
-    /// calc E[x^2]
+    /// calc `E[x^2]`
     pub fn xx(&self) -> f64 {
         let u = self.u.into();
         let l = self.l.into();
         (u * u + u * l + l * l) / 3.0f64
     }
-    /// calc E[cos(x)]
+    /// calc `E[cos(x)]`
     pub fn c(&self) -> f64 {
         let u = self.u.into();
         let l = self.l.into();
         (u.sin() - l.sin()) / (u - l)
     }
-    // calc E[sin(x)]
+    /// calc `E[sin(x)]`
     pub fn s(&self) -> f64 {
         let u = self.u.into();
         let l = self.l.into();
