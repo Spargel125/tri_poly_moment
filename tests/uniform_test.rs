5b8e91f8bcb26e1fcb6275e0b49c4b963a092a22
spargel125 <asupar125ce@gmail.com>
2023-08-27 22:17:46 +0900

    Uniform 完成

--- tests/uniform_test.rs
+++ tests/uniform_test.rs
@@ -8,36 +8,52 @@ mod tests_uniform {
     #[test]
     fn test_int() {
         // Test case 1
-        let uniform_int = Uniform::new(3, 9);
-        assert!(uniform_int.x() - 6.0 < 1e-12);
-        assert!(uniform_int.xx() - 39.0 < 1e-12);
-        assert!(uniform_int.c() - 0.045166412863648 < 1e-12);
-        assert!(uniform_int.s() - (-0.013143705785961) < 1e-12);
-        assert!(uniform_int.xc() - 0.560761429618663 < 1e-12);
-        assert!(uniform_int.xs() - 0.916865557390441 < 1e-12);
-        assert!(uniform_int.xxc() - 3.518188423893031 < 1e-12);
-        assert!(uniform_int.xxs() - 11.936792649779795 < 1e-12);
-        assert!(uniform_int.cc() - 0.480351177142802 < 1e-12);
-        assert!(uniform_int.ss() - 0.916865557390441 < 1e-12);
-        assert!(uniform_int.sc() - 0.012493899100262 < 1e-12);
-        assert!(uniform_int.cs() - 0.012493899100262 < 1e-12);
+        let uniform = Uniform::new(3, 9);
+        assert!(uniform.x() - 6.0 < 1e-12);
+        assert!(uniform.xx() - 39.0 < 1e-12);
+        assert!(uniform.c() - 0.045166412863648 < 1e-12);
+        assert!(uniform.s() - (-0.013143705785961) < 1e-12);
+        assert!(uniform.xc() - 0.560761429618663 < 1e-12);
+        assert!(uniform.xs() - 0.916865557390441 < 1e-12);
+        assert!(uniform.xxc() - 3.518188423893031 < 1e-12);
+        assert!(uniform.xxs() - 11.936792649779795 < 1e-12);
+        assert!(uniform.cc() - 0.480351177142802 < 1e-12);
+        assert!(uniform.ss() - 0.916865557390441 < 1e-12);
+        assert!(uniform.sc() - 0.012493899100262 < 1e-12);
+        assert!(uniform.cs() - 0.012493899100262 < 1e-12);
     }
     #[test]
     fn test_float() {
         // Test case 2
-        let uniform_float = Uniform::new(-2.1, 5.4);
-        assert!(uniform_float.x() - 1.65 < 1e-12);
-        assert!(uniform_float.xx() - 7.41 < 1e-12);
-        assert!(uniform_float.c() - 0.012059317212385 < 1e-12);
-        assert!(uniform_float.s() - (-0.151938530738999) < 1e-12);
+        let uniform = Uniform::new(-2.1, 5.4);
+        assert!(uniform.x() - 1.65 < 1e-12);
+        assert!(uniform.xx() - 7.41 < 1e-12);
+        assert!(uniform.c() - (0.012059317212385) < 1e-12);
+        assert!(uniform.s() - (-0.151938530738999) < 1e-12);
+        assert!(uniform.xc() - (-0.646150522962996) < 1e-12);
+        assert!(uniform.xs() - (-0.303562644178352) < 1e-12);
+        assert!(uniform.xxc() - (-1.889815931671436) < 1e-12);
+        assert!(uniform.xxs() - (-4.056836457095673) < 1e-12);
+        assert!(uniform.cc() - (0.438249599917331) < 1e-12);
+        assert!(uniform.ss() - (0.561750400082669) < 1e-12);
+        assert!(uniform.sc() - (-0.009864363829512) < 1e-12);
+        assert!(uniform.cs() - (-0.009864363829512) < 1e-12);
     }
     #[test]
     fn test_int_float() {
         // Test case 3
-        let uniform_int_float = Uniform::new(-13.3, 11);
-        assert!(uniform_int_float.x() - (-1.15) < 1e-12);
-        assert!(uniform_int_float.xx() - 50.53 < 1e-12);
-        assert!(uniform_int_float.c() - 0.013597549150375 < 1e-12);
-        assert!(uniform_int_float.s() - 0.030383682087062 < 1e-12);
+        let uniform = Uniform::new(-13.3, 11);
+        assert!(uniform.x() - (-1.15) < 1e-12);
+        assert!(uniform.xx() - 50.53 < 1e-12);
+        assert!(uniform.c() - 0.013597549150375 < 1e-12);
+        assert!(uniform.s() - 0.030383682087062 < 1e-12);
+        assert!(uniform.xc() - (-0.849526484937785) < 1e-12);
+        assert!(uniform.xs() - (-0.422126218896357) < 1e-12);
+        assert!(uniform.xxc() - (0.738959443649581) < 1e-12);
+        assert!(uniform.xxs() - (3.685695659877433) < 1e-12);
+        assert!(uniform.cc() - (0.510141908071887) < 1e-12);
+        assert!(uniform.ss() - (0.489858091928113) < 1e-12);
+        assert!(uniform.sc() - (0.011350961867270) < 1e-12);
+        assert!(uniform.cs() - (0.011350961867270) < 1e-12);
     }
 }
